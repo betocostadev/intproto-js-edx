39a7ac3e67d4b6b0ae93c2c9e25e7bfe7194e9eb
Beto Costa <robertomarx@gmail.com>
2018-09-14 16:40:41 -0300

    first prototype working

--- js/guess-game.js
+++ js/guess-game.js
@@ -1,1 +1,27 @@
 /* Guessing Game */
+// program data:
+const number = 4;
+let guess;
+let limit = 3;
+let won =  false;
+
+for (i = 1; i <= limit; i++){
+    // prompt user for their guess
+    guess = prompt("Guess a number");
+
+    // if correct: let the user know they won
+    if(guess == number){
+        document.write("Correct! You won.");
+        won = true;
+        break;
+    }
+    // if incorrect: let the user know how many tries they have remaining
+    else{
+        alert("incorrect. \nTries remaining: " + (limit - i));
+    }
+}
+
+// if the user ran out of tries, let them know the game is over
+if(!won){
+    document.write("Sorry, you ran out of tries. Game over.");
+}
