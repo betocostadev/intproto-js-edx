a2ae200a60b9b07f224add72e8d0d17e726d6d45
Beto Costa <robertomarx@gmail.com>
2018-09-17 11:47:54 -0300

    first version of the Guess Game done

--- js/guess-game.js
+++ js/guess-game.js
@@ -1,40 +1,62 @@
 /* Guessing Game */
 // program data:
-const number = 4;
-let guess;
-let limit = 3;
-let won =  false;
-let guesses = [];
+const startBtn = document.getElementById('start-game');
+const result = document.getElementById('result');
 
-for (i = 1; i <= limit; i++){
-    // prompt user for their guess
-    guess = parseInt(prompt("Guess a number: "));
+startBtn.addEventListener('click', function () {
+    const number = Math.floor(Math.random() * 100) + 1;
+    let guess;
+    let limit = 5;
+    let won =  false;
+    let guesses = [];
 
-    // if correct: let the user know they won
-    if(guess == number){
-        document.write("Correct! You won.");
-        won = true;
-        break;
-    } else if (isPreviousGuess() == true) {
-        alert(`Number already guessed. You have guessed ${guesses}, and you still have ${limit - i} remaining guesses.`);
-    } else {
-        // if incorrect: let the user know how many tries they have remaining
-        guesses[i] = guess;
-        alert("incorrect. \nYou have guessed: " + guesses.toString() + "\nTries remaining: " + (limit - i));
-    }
-}
 
-// if the user ran out of tries, let them know the game is over
-if(!won){
-    document.write("Sorry, you ran out of tries. Game over.");
-}
+    for (i = 1; i <= limit; i++){
+        // prompt user for their guess
+        do {
+            guess = parseInt(prompt("Guess a number between 1 and 100: "));
+        } while (isNaN(guess) || (guess > 100) || (guess <= 0));
 
-// Check if the user has already guessed the number:
-function isPreviousGuess(){
-    for(i = 0; i < guesses.length; i++){
-        if(guesses[i] == guess){
-            return true;
+        // if correct: let the user know they won
+        if(guess == number){
+            result.innerHTML = ("Correct! You won.");
+            won = true;
+            break;
+        } else if (isPreviousGuess() == true) {
+            alert(`Number already guessed. You have guessed the numbers ${guesses}, and you still have ${limit - i} remaining guesses.`);
+        } else if (guess > number) {
+            guesses[i] = guess;
+            alert(`Incorret number!
+            You have guessed: ${guesses}.
+            The number is lower!
+            Tries remaining: ${limit - i}`);
+        } else if (guess < number) {
+            guesses[i] = guess;
+            alert(`Incorret number!
+            You have guessed: ${guesses}.
+            The number is higher!
+            Tries remaining: ${limit - i}`);
+        } else {
+            // if incorrect: let the user know how many tries they have remaining
+            guesses[i] = guess;
+            alert("Incorrect number. \nYou have guessed: " + guesses.toString() + "\nTries remaining: " + (limit - i));
         }
     }
-    return false;
-}
+
+    // if the user ran out of tries, let them know the game is over
+    if(!won){
+        result.innerHTML = ("The number was " + number + ". Sorry, you ran out of tries.<br>" + "Your guesses were " + guesses + "<br> Game over.");
+        // document.write("The number was " + number + ". Sorry, you ran out of tries.<br>" + "Your guesses were " + guesses + "<br> Game over.");
+    }
+
+    // Check if the user has already guessed the number:
+    function isPreviousGuess(){
+        for(i = 0; i < guesses.length; i++){
+            if(guesses[i] == guess){
+                return true;
+            }
+        }
+        return false;
+    }
+
+});
