ea5394efb916f2dae3a0a563d26b5746bd5841bf
Beto Costa <robertomarx@gmail.com>
2018-09-17 07:44:58 -0300

    added check if number already guessed

--- js/guess-game.js
+++ js/guess-game.js
@@ -4,24 +4,37 @@ const number = 4;
 let guess;
 let limit = 3;
 let won =  false;
+let guesses = [];
 
 for (i = 1; i <= limit; i++){
     // prompt user for their guess
-    guess = prompt("Guess a number");
+    guess = parseInt(prompt("Guess a number: "));
 
     // if correct: let the user know they won
     if(guess == number){
         document.write("Correct! You won.");
         won = true;
         break;
-    }
-    // if incorrect: let the user know how many tries they have remaining
-    else{
-        alert("incorrect. \nTries remaining: " + (limit - i));
+    } else if (isPreviousGuess() == true) {
+        alert(`Number already guessed. You have guessed ${guesses}, and you still have ${limit - i} remaining guesses.`);
+    } else {
+        // if incorrect: let the user know how many tries they have remaining
+        guesses[i] = guess;
+        alert("incorrect. \nYou have guessed: " + guesses.toString() + "\nTries remaining: " + (limit - i));
     }
 }
 
 // if the user ran out of tries, let them know the game is over
 if(!won){
     document.write("Sorry, you ran out of tries. Game over.");
+}
+
+// Check if the user has already guessed the number:
+function isPreviousGuess(){
+    for(i = 0; i < guesses.length; i++){
+        if(guesses[i] == guess){
+            return true;
+        }
+    }
+    return false;
 }
